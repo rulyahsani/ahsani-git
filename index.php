9b6cdb40f6bb211be3a84fe11d05349fbc0f92a9
ahsaniyusron <tukangjaringan20@gmail.com>
2020-07-24 10:10:50 +0700

    modifikasi file lagi

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello World
+    <h2>Git Hello World</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, laudantium, explicabo id praesentium dolore atque eum exercitationem asperiores reprehenderit consequuntur mollitia incidunt, reiciendis delectus architecto molestiae aspernatur. Natus, non eius!</p>
+</body>
+</html>
